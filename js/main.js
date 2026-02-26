769c624d04736645973fbc0f41a57fbba0ca0582
ashishkekan <ashking201299@gmail.com>
2026-02-26 22:31:18 +0530

    Changes in design

--- js/main.js
+++ js/main.js
@@ -395,7 +395,7 @@ function renderNavbar() {
   
   const navHTML = `
     <nav class="navbar glass">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between h-16">
           <!-- Logo -->
           <a href="index.html" class="flex items-center gap-2 group">
