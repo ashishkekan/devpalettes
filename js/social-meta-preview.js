9f875c3aea0656b63f1408072ed70a058db79f08
ashish <ashish.kekaan@lemonlabs.in>
2026-04-10 18:34:22 +0530

    Update and optimized seo

--- js/social-meta-preview.js
+++ js/social-meta-preview.js
@@ -15,6 +15,22 @@
        document.getElementById('tw-title').textContent = title;
        document.getElementById('tw-desc').textContent = desc;
        document.getElementById('tw-img').style.backgroundImage = `url(${img})`;
+
+       document.getElementById('linkedin-title').textContent = title;
+       document.getElementById('linkedin-desc').textContent = desc;
+       document.getElementById('linkedin-img').style.backgroundImage = `url(${img})`;
+
+       document.getElementById('whatsapp-title').textContent = title;
+       document.getElementById('whatsapp-desc').textContent = desc;
+       document.getElementById('whatsapp-img').style.backgroundImage = `url(${img})`;
+
+       document.getElementById('pinterest-title').textContent = title;
+       document.getElementById('pinterest-desc').textContent = desc;
+       document.getElementById('pinterest-img').style.backgroundImage = `url(${img})`;
+
+       document.getElementById('slack-title').textContent = title;
+       document.getElementById('slack-desc').textContent = desc;
+       document.getElementById('slack-img').style.backgroundImage = `url(${img})`;
     };
     
     updatePreview();
