7b1e6bdd2010933fb9708f4f62d8d3b6b1f4a0d7
ashishkekan <ashking201299@gmail.com>
2026-04-21 20:50:44 +0530

    Update logo design

--- js/main.js
+++ js/main.js
@@ -671,13 +671,13 @@ function renderNavbar() {
           <!-- Logo -->
           <a href="${navHref('')}" class="flex items-center gap-2 group">
             <div class="flex items-center gap-2 sm:gap-3 group cursor-pointer">
-              <div class="w-8 h-8 sm:w-8 sm:h-8 rounded-lg sm:rounded-xl bg-black
+              <div class="w-7 h-7 sm:w-7 sm:h-7 rounded-lg sm:rounded-xl bg-black
                 flex items-center justify-center
                 shadow-[0_0_15px_rgba(34,211,238,0.6)]
                 transition group-hover:shadow-[0_0_25px_rgba(34,211,238,1)] overflow-hidden">
                 <img src="${navHref('images/devpalettes_zoom_180.png')}" 
                   alt="Devpalettes Logo"
-                  class="w-6 h-6 sm:w-6 sm:h-6 object-contain mx-auto" />
+                  class="w-5 h-5 sm:w-5 sm:h-5 object-contain mx-auto"/>
               </div>
               <span class="text-lg sm:text-2xl font-bold text-cyan-400">
                 Devpalettes
@@ -803,13 +803,13 @@ function renderFooter() {
         <div class="border-t border-slate-200 dark:border-slate-800 mt-8 sm:mt-12 pt-6 sm:pt-8 flex flex-col sm:flex-row justify-between items-center gap-4">
           <a href="/" class="flex items-center gap-2 group mb-4 sm:mb-0">
             <div class="flex items-center gap-2 sm:gap-3 group cursor-pointer">
-              <div class="w-8 h-8 sm:w-8 sm:h-8 rounded-lg sm:rounded-xl bg-black
+              <div class="w-7 h-7 sm:w-7 sm:h-7 rounded-lg sm:rounded-xl bg-black
                 flex items-center justify-center
                 shadow-[0_0_15px_rgba(34,211,238,0.6)]
                 transition group-hover:shadow-[0_0_25px_rgba(34,211,238,1)] overflow-hidden">
                 <img src="/images/devpalettes_zoom_180.png" 
                   alt="Devpalettes Logo"
-                  class="w-6 h-6 sm:w-6 sm:h-6 object-contain mx-auto" />
+                  class="w-5 h-5 sm:w-5 sm:h-5 object-contain mx-auto" />
               </div>
               <span class="text-xl sm:text-2xl font-bold text-cyan-400">
                 Devpalettes
